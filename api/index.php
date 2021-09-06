9d161e0b53e2a4bae056adbbec6c3474d3285819
Kamo28-cd <83655650+Kamo28-cd@users.noreply.github.com>
2021-09-06 08:09:48 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php 
 require_once("DB.php");
 
-//$db = new DB("127.0.0.1", "studenthub", "root", "Epsilion28");
-$db = new DB('127.0.0.1', 'buble', 'root', '');
-//$db = new DB("localhost:3306", "bublecoz_bublehub", "bublecoz_Kamo", "Epsilion28.");
+
+$db = new DB('127.0.0.1', 'dbname', 'username', 'password');
+
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 	 if ($_GET['url'] == "musers") {
